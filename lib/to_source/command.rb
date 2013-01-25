b844d2b7c1e8344402c74b6b76878d174c33df7f
Markus Schirp <mbj@seonic.net>
2013-01-25 11:20:33 +0100

    Add yard docs for command

--- lib/to_source/command.rb
+++ lib/to_source/command.rb
@@ -1,44 +1,91 @@
 module ToSource
+  # Abstract base class for emitter command
   class Command
     include Adamantium::Flat
 
+    # A null command
     NULL = Class.new(self) do
+
+      # Run command
+      #
+      # @param [State] _state
+      #
+      # @return [self]
+      #
+      # @api private
+      #
       def run(_state)
+        self
       end
     end.new.freeze
 
+    # Command that emits token
     class Token < self
       attr_reader :content
 
+      # Run command
+      #
+      # @param [State] state
+      #
+      # @return [self]
+      #
       def run(state)
         state.push(self)
+        self
       end
 
     private
 
+      # Initialize object
+      #
+      # @param [String] content
+      #
+      # @return [undefined]
+      #
+      # @api private
+      #
       def initialize(content)
         @content = content
       end
+
     end
 
+    # Command that does a shift
     class Shift < self
       include Equalizer.new(:width)
 
       attr_reader :width
 
+      # Run command
+      #
+      # @param [State]
+      #
+      # @return [self]
+      #
+      # @api private
+      #
       def run(state)
         state.shift(width)
+        self
       end
 
     private
 
+      # Initialize command
+      #
+      # @param [Fixnum] with
+      #
+      # @return [undefined]
+      #
+      # @api private
+      #
       def initialize(width)
         @width = width
       end
 
       INDENT   = Shift.new( 2)
       UNINDENT = Shift.new(-2)
-
     end
+
   end
 end
