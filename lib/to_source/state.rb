a2c877e29b00ecd5c0a17f6fc6dfa46ed4fabfe6
Markus Schirp <mbj@seonic.net>
2013-01-25 11:22:09 +0100

    Add Yard docs for state

--- lib/to_source/state.rb
+++ lib/to_source/state.rb
@@ -1,55 +1,137 @@
 module ToSource
+  # Emitter state
   class State
 
+    # Return last command
+    #
+    # @return [Command]
+    #
+    # @api private
+    #
     attr_reader :last
+
+    # Return current indentation level
+    #
+    # @return [Fixnum]
+    #
+    # @api private
+    #
     attr_reader :identation
+
+    # Return buffer
+    #
+    # @return [Array]
+    #
+    # @api private
+    #
     attr_reader :buffer
 
+    # Initialize object
+    #
+    # @return [undefined]
+    #
+    # @api private
+    #
     def initialize
       @last        = Command::NULL
       @indentation = 0
       @buffer      = []
     end
 
+    # Execute command 
+    #
+    # @param [Command] commant
+    #
+    # @return [self]
+    #
+    # @api private
+    #
     def execute(command)
       command.run(self)
       @last = command
+      self
     end
 
-    def last_keyword?
-      last.kind_of?(Command::Token::Keyword)
-    end
-
+    # Write string to buffer
+    #
+    # @param [String] string
+    #
+    # @return [self]
+    #
+    # @api private
+    #
     def write(string)
       @buffer << string
+      self
     end
 
-    def space
-      write(' ')
-    end
-
-    def push(command)
-      indent
-      write(command.content)
-    end
-
-    def indent
-      return unless blank?
-      write(' ' * @indentation)
-    end
-
-    def blank?
-      buffer.last == "\n"
-    end
-
-    def new_line
-      write("\n")
-    end
-
+    # Return source
+    #
+    # @return [String]
+    # 
+    # @api private
+    #
     def source
       buffer.join('')
     end
 
+    # Push command
+    #
+    # @param [Command] command
+    #
+    # @return [self]
+    #
+    # @api private
+    #
+    def push(command)
+      indent
+      write(command.content)
+      self
+    end
+ 
+    # Indent line if needed
+    #
+    # @return [self]
+    #
+    # @api private
+    #
+    def indent
+      return unless blank?
+      write(' ' * @indentation)
+      self
+    end
+
+    # Test for blank line
+    #
+    # @return [true]
+    #   if line is blank
+    #
+    # @return [false]
+    #   otherwise
+    #
+    def blank?
+      buffer.last == "\n"
+    end
+
+    # Write newline
+    #
+    # @return [self]
+    #
+    # @api private
+    #
+    def new_line
+      write("\n")
+      self
+    end
+
+    # Perform shift by width
+    #
+    # @param [Fixnum] width
+    #
+    # @return [self]
+    #
+    # @api private
+    #
     def shift(width)
       @indentation += width
       @indentation = 0 if @indentation < 0
