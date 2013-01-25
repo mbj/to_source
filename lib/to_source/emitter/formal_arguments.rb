c90361a0cf0d4c460a84d65283f1d4a5ea6526ab
Markus Schirp <mbj@seonic.net>
2013-01-25 12:13:29 +0100

    Add yard coverage for formal arguments emitter

--- lib/to_source/emitter/formal_arguments.rb
+++ lib/to_source/emitter/formal_arguments.rb
@@ -1,85 +1,16 @@
 module ToSource
   class Emitter
+    # Emitter for formal arguments
     class FormalArguments < self
 
     private
 
-      def any?
-        required? or defaults? or splat? or block_arg?
-      end
-
-      def required
-        arguments.required
-      end
-
-      def required?
-        required.any?
-      end
-
-      def defaults
-        arguments.defaults
-      end
-
-      def defaults?
-        !!defaults
-      end
-
-      def splat
-        arguments.splat
-      end
-
-      def splat?
-        !!splat
-      end
-
-      def block_arg
-        arguments.block_arg
-      end
-
-      def block_arg?
-        !!block_arg
-      end
-
-      def emit_required
-        array = required
-        max = array.length - 1
-        array.each_with_index do |member, index|
-          if member.kind_of?(Rubinius::AST::Node)
-            visit(member)
-          else
-            emit(member)
-          end
-          emit(', ') if index < max
-        end
-      end
-
-      def emit_defaults
-        return unless defaults?
-        emit(', ') if required?
-        visit(defaults)
-      end
-
-      def emit_splat
-        return unless splat?
-
-        emit(', ') if required? or defaults?
-        emit('*')
-        value = splat
-        unless value == :@unnamed_splat
-          emit(value)
-        end
-      end
-
-      def emit_block_arg
-        return unless block_arg?
-        emit(', ') if required? or defaults? or splat?
-        visit(block_arg)
-      end
-
-      def arguments
-        node.arguments
-      end
-
+      # Perform dispatch
+      #
+      # @return [undefined]
+      #
+      # @api private
+      #
       def dispatch
         return unless any?
 
@@ -95,18 +26,196 @@ module ToSource
         emit(util::CLOSE)
       end
 
+      # Test if any argument is present
+      #
+      # @return [true]
+      #   if any argument is present
+      #
+      # @return [false]
+      #   otherwise
+      #
+      # @api private
+      #
+      def any?
+        required? or defaults? or splat? or block_arg?
+      end
+
+      delegate :arguments
+
+      # Return required
+      #
+      # @return [Array]
+      #
+      # @api private
+      #
+      def required
+        arguments.required
+      end
+
+      # Test if any required arguments are present
+      #
+      # @return [true]
+      #   if any required arguments are presnet
+      #
+      # @return [false]
+      #   otherwise
+      #
+      # @api private
+      #
+      def required?
+        required.any?
+      end
+
+      # Return defaults
+      #
+      # @return [Rubinius::AST::Node]
+      #
+      # @api private
+      #
+      def defaults
+        arguments.defaults
+      end
+
+      # Test if defaults are present
+      #
+      # @return [true]
+      #   if defaults are present
+      #
+      # @return [false]
+      #   otherwise
+      #
+      def defaults?
+        !!defaults
+      end
+
+      # Return splat arguments
+      #
+      # @return [Rubinius::AST::Node]
+      #
+      # @api private
+      #
+      def splat
+        arguments.splat
+      end
+
+      # Test if splat arguments are present
+      #
+      # @return [true]
+      #   if splat arguments are present
+      #
+      # @return [false]
+      #   otherwise
+      #
+      # @api private
+      #
+      def splat?
+        !!splat
+      end
+
+      # Return block argument
+      #
+      # @return [Rubinius::AST::Node]
+      #
+      # @api private
+      #
+      def block_arg
+        arguments.block_arg
+      end
+
+      # Test if block argument is presnet
+      #
+      # @return [true]
+      #   if block argument is present
+      #
+      # @return [false]
+      #   otherwise
+      #
+      def block_arg?
+        !!block_arg
+      end
+
+      # Emit required
+      #
+      # @return [undefined]
+      #
+      # @api private
+      #
+      def emit_required
+        array = required
+        max = array.length - 1
+        array.each_with_index do |member, index|
+          if member.kind_of?(Rubinius::AST::Node)
+            visit(member)
+          else
+            emit(member)
+          end
+          emit(', ') if index < max
+        end
+      end
+
+      # Emit defaults
+      #
+      # @return [undefined]
+      #
+      # @api private
+      #
+      def emit_defaults
+        return unless defaults?
+        emit(', ') if required?
+        visit(defaults)
+      end
+
+      # Emit splat
+      #
+      # @return [undefined]
+      #
+      # @api private
+      #
+      def emit_splat
+        return unless splat?
+
+        emit(', ') if required? or defaults?
+        emit('*')
+        value = splat
+        unless value == :@unnamed_splat
+          emit(value)
+        end
+      end
+
+      # Emit block arg
+      #
+      # @return [undefined]
+      #
+      # @api private
+      #
+      def emit_block_arg
+        return unless block_arg?
+        emit(', ') if required? or defaults? or splat?
+        visit(block_arg)
+      end
+
+      # Emmiter for block arguments
       class Block < self
         OPEN = ' |'.freeze
         CLOSE = '|'.freeze
       end
 
+      # Emitter for method arguments
       class Method < self
         OPEN = '('.freeze
         CLOSE = ')'.freeze
 
+        # Generic emitter for method arguments
         class Generic < self
+
           handle(Rubinius::AST::FormalArguments19)
 
+        private
+
+          # Return arguments
+          #
+          # @return [Rubnius::AST::Node]
+          #
           def arguments
             node
           end
@@ -114,7 +223,6 @@ module ToSource
         end
 
       end
-
     end
   end
 end
