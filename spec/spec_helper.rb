011884814eb9a986dfe145b2335c5fb9300e5e84
Markus Schirp <mbj@seonic.net>
2013-02-24 20:00:37 +0100

    Use Devtools.init_spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,7 @@
 # encoding: utf-8
 
-require 'rspec'
-
-# require spec support files and shared behavior
-Dir[File.expand_path('../{support,shared}/**/*.rb', __FILE__)].each { |f| require f }
-
-require 'to_source'
+require 'devtools'
+Devtools.init_spec_helper
 
 RSpec.configure do |config|
 end
